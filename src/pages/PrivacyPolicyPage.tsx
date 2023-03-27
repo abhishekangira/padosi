606da543ee877301933a5fea1ae41ae4b81b927c
Abhishek Angira <abhishek@angira.net>
2023-03-27 13:11:12 +0530

    added TOU and privacy policy

---
+++ src/pages/PrivacyPolicyPage.tsx
@@ -0,0 +1,93 @@
+import Head from "next/head";
+
+export function PrivacyPolicyPage() {
+  return (
+    <>
+      <Head>
+        <title>Privacy Policy</title>
+        <meta name="description" content="Privacy policy for our website" />
+      </Head>
+      <div className="container mx-auto px-4 py-8">
+        <h1 className="mb-4 text-3xl font-bold dark:text-red-500">
+          Privacy Policy
+        </h1>
+        <p className="mb-4">
+          This privacy policy sets out how we collect, use, protect and share
+          personal information that we obtain from users of our website.
+        </p>
+
+        <h2 className="mb-2 text-xl font-semibold">
+          Personal Information We Collect
+        </h2>
+        <p className="mb-4">
+          We collect personal information from users who sign up on our website,
+          including their name, email address and location. We do not collect
+          personal information from users who do not sign up.
+        </p>
+
+        <h2 className="mb-2 text-xl font-semibold">
+          How We Collect Personal Information
+        </h2>
+        <p className="mb-4">
+          We collect personal information through our sign-up process, which
+          requires users to provide their name, email address and location.
+        </p>
+
+        <h2 className="mb-2 text-xl font-semibold">
+          How We Use Personal Information
+        </h2>
+        <p className="mb-4">
+          We use personal information to improve the user experience on our
+          website. Location information is used to display posts from nearby
+          users, and email/phone numbers are used as user identification numbers
+          (UIDs). Names are displayed on user profiles and posts.
+        </p>
+
+        <h2 className="mb-2 text-xl font-semibold">
+          Sharing of Personal Information
+        </h2>
+        <p className="mb-4">
+          We do not share personal information with any third parties.
+        </p>
+
+        <h2 className="mb-2 text-xl font-semibold">
+          How We Protect Personal Information
+        </h2>
+        <p className="mb-4">
+          We store personal information in a secure database to protect it from
+          unauthorized access or disclosure.
+        </p>
+
+        <h2 className="mb-2 text-xl font-semibold">
+          How Long We Keep Personal Information
+        </h2>
+        <p className="mb-4">
+          We keep personal information for as long as the user's account remains
+          active. Users can delete their account at any time.
+        </p>
+
+        <h2 className="mb-2 text-xl font-semibold">User Rights</h2>
+        <p className="mb-4">
+          Users have the right to edit and update their personal information.
+          They also have the right to request the deletion of their account,
+          which will result in the deletion of all personal information
+          associated with their account.
+        </p>
+
+        <h2 className="mb-2 text-xl font-semibold">
+          Handling Data Breaches or Security Incidents
+        </h2>
+        <p className="mb-4">
+          In the event of a data breach or security incident, we will contact
+          local law enforcement authorities and take appropriate measures to
+          mitigate any harm to affected users.
+        </p>
+
+        <p className="mb-4">
+          If you have any questions or concerns about our privacy policy, please
+          contact us at abhishek@angira.net.
+        </p>
+      </div>
+    </>
+  );
+}
---
+++ src/pages/TermsOfUsePage.tsx
@@ -0,0 +1,96 @@
+export function TermsOfUsePage() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="mb-4 text-3xl font-bold">Terms of Use</h1>
+      <p className="mb-4">
+        Welcome to Padosi (the “Website”). By accessing and using the Website,
+        you agree to be bound by the following Terms of Use (“Terms”), as well
+        as our Privacy Policy, which is incorporated herein by reference. If you
+        do not agree to these Terms or the Privacy Policy, please do not use the
+        Website.
+      </p>
+      <p className="mb-4">
+        1. Use of Website The Website is intended for use by individuals who are
+        at least 13 years of age or older. By using the Website, you represent
+        and warrant that you are at least 13 years of age. The Website may not
+        be used for any illegal or unauthorized purpose. You agree to comply
+        with all applicable laws and regulations when using the Website.
+      </p>
+      <p className="mb-4">
+        2. Intellectual Property The content on the Website, including without
+        limitation, the text, graphics, photos, software, and other materials,
+        as well as the selection, organization, coordination, compilation, and
+        overall look and feel of the Website, are protected by copyright,
+        trademark, and other laws of the United States and other countries. All
+        rights reserved. You may not reproduce, distribute, modify, create
+        derivative works of, publicly display, publicly perform, republish,
+        download, store, or transmit any of the material on our Website, except
+        as follows:
+      </p>
+      <p className="mb-4">
+        If we provide desktop, mobile, or other applications for download, you
+        may download a single copy to your computer or mobile device solely for
+        your own personal, non-commercial use, provided you agree to be bound by
+        our end user license agreement for such applications.
+      </p>
+      <p className="mb-4">
+        3. Disclaimer of Warranties The Website is provided on an “as is” and
+        “as available” basis without any representations or warranties of any
+        kind, express or implied, as to the operation of the Website or the
+        information, content, materials, or products included on the Website. To
+        the fullest extent permissible by applicable law, we disclaim all
+        warranties, express or implied, including, but not limited to, implied
+        warranties of merchantability and fitness for a particular purpose.
+      </p>
+      <p className="mb-4">
+        4. Limitation of Liability To the fullest extent permissible by
+        applicable law, we will not be liable for damages of any kind arising
+        from the use of the Website, including, but not limited to, direct,
+        indirect, incidental, punitive, and consequential damages. By using the
+        Website, you agree to indemnify and hold us harmless from any and all
+        claims, liabilities, damages, losses, or expenses, including attorneys’
+        fees and costs, arising out of your use of the Website or your breach of
+        these Terms.
+      </p>
+      <p className="mb-4">
+        5. Modifications to Terms We reserve the right to modify these Terms at
+        any time without prior notice. Your continued use of the Website after
+        any modification to these Terms constitutes your acceptance of the new
+        Terms.
+      </p>
+      <p className="mb-4">
+        6. Governing Law and Jurisdiction These Terms and any dispute arising
+        out of or related to the use of the Website shall be governed by and
+        construed in accordance with the laws of the state or jurisdiction where
+        our company is located, without regard to its conflict of law
+        provisions. Any legal action or proceeding relating to the use of the
+        Website shall be brought exclusively in a federal or state court of
+        competent jurisdiction located in the state or jurisdiction where our
+        company is located.
+      </p>
+      <p className="mb-4">
+        7. Termination We reserve the right to terminate your access to the
+        Website at any time, without notice, for any reason, including, without
+        limitation, if we believe that you have violated these Terms.
+      </p>
+      <p className="mb-4">
+        8. Entire Agreement These Terms and any dispute arising out of or
+        related to the use of the Website shall be governed by and construed in
+        accordance with the laws of the state or jurisdiction where our company
+        is located, without regard to its conflict of law provisions. Any legal
+        action or proceeding relating to the use of the Website shall be brought
+        exclusively in a federal or state court of competent jurisdiction
+        located in the state or jurisdiction where our company is located.
+      </p>
+      <p className="mb-4">
+        9. Contact Information If you have any questions or comments about these
+        Terms, please contact us at [insert contact information].
+      </p>
+      <p className="text-sm text-gray-500">
+        By accessing and using the Website, you acknowledge that you have read,
+        understood, and agree to be bound by these Terms and our Privacy Policy.
+        Thank you for visiting our Website.
+      </p>
+    </div>
+  );
+}
