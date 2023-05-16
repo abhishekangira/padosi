0ac2a7e2b1f62a7f4ec822785be2533d2e0a185d
Abhishek Angira <abhishek@angira.net>
2023-05-16 14:23:44 +0530

    changed firebase auth domain

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -5,7 +5,7 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "padosi-webapp.firebaseapp.com",
+  authDomain: "padosi.me",
   projectId: "padosi-webapp",
   storageBucket: "padosi-webapp.appspot.com",
   messagingSenderId: "202304249193",
